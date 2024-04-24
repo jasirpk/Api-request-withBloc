354a42eb52a2816eff35aad482449bbae503624e
jasir <jasijasu959@gmail.com>
2024-04-24 18:30:27 +0530

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:public_api_bloc/bloc/logic_bloc.dart';
 import 'package:public_api_bloc/screens/posts_page.dart';
 
 void main() {
-  runApp(PUblicApi());
+  runApp(BlocProvider(
+    create: (context) => LogicBloc(),
+    child: PUblicApi(),
+  ));
 }
 
 class PUblicApi extends StatelessWidget {
